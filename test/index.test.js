469a6921b81d706f25c70b5f03c725730fde0fcc
Johannes Ewald <johannes.ewald@peerigon.com>
2017-03-16 15:50:40 +0100

    Update tests to jest

---
+++ test/index.test.js
@@ -0,0 +1,87 @@
+const compile = require('./helpers/compile');
+const { readCssFixture, readSourceMap } = require('./helpers/readFixture');
+
+async function compileAndCompare(fixture, loaderOptions, loaderContext) {
+  const [result, expectedCss] = await Promise.all([
+    compile(fixture, loaderOptions, loaderContext),
+    readCssFixture(fixture),
+  ]);
+  const [actualCss] = result.inspect.arguments;
+
+  return expect(actualCss).toBe(expectedCss);
+}
+
+test('should compile simple less without errors', async () => {
+  await compileAndCompare('basic');
+});
+
+test('should resolve all imports', async () => {
+  await compileAndCompare('imports');
+});
+
+test('should resolve all imports from node_modules', async () => {
+  await compileAndCompare('imports-node');
+});
+
+test('should not try to resolve import urls', async () => {
+  await compileAndCompare('imports-url');
+});
+
+test('should compile data-uri function', async () => {
+  await compileAndCompare('data-uri');
+});
+
+test('should transform urls', async () => {
+  await compileAndCompare('url-path');
+});
+
+test('should transform urls to files above the current directory', async () => {
+  await compileAndCompare('folder/url-path');
+});
+
+test('should transform urls to files above the sibling directory', async () => {
+  await compileAndCompare('folder2/url-path');
+});
+
+test('should generate source maps', async () => {
+  const [{ inspect }, expectedSourceMap] = await Promise.all([
+    compile('source-map', { sourceMap: true }),
+    readSourceMap('source-map'),
+  ]);
+
+  const map = JSON.parse(inspect.arguments[1]);
+
+  delete map.sourcesContent;
+
+  expect(JSON.stringify(map)).toEqual(expectedSourceMap);
+});
+
+test('should install plugins', async () => {
+  let pluginInstalled = false;
+  const testPlugin = {
+    install() {
+      pluginInstalled = true;
+    },
+  };
+
+  await compile('basic', { plugins: [testPlugin] });
+
+  expect(pluginInstalled).toBe(true);
+});
+
+test('should not alter the original options object', async () => {
+  const options = { plugins: [] };
+  const copiedOptions = { ...options };
+
+  await compile('basic', options);
+
+  expect(copiedOptions).toEqual(options);
+});
+
+test('should report error correctly', async () => {
+  const err = await compile('error')
+    .catch(e => e);
+
+  expect(err).toBeInstanceOf(Error);
+  expect(err.message).toMatch(/not-existing/);
+});
