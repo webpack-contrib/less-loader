5297675254c7f25d766cd4b50631cfcd50a131d9
Johannes Ewald <johannes.ewald@peerigon.com>
2017-03-20 19:21:11 +0100

    docs: add source map example

---
+++ examples/sourceMaps/webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  entry: require.resolve('./main.less'),
+  output: {
+    filename: 'bundle.js',
+  },
+  devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: require.resolve('../../dist'),
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    inline: true,
+  },
+};
