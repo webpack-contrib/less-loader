e378456f6f4ce9cdd332f59a560561088535bf92
Joshua Wiens <joshuaw@easymetrics.com>
2017-12-21 03:28:24 -0600

    refactor: Enforce options schema
    BREAKING CHANGE: Less Loader now enforces it's options & types, incorrect configurations with throw.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import less from 'less';
 import pify from 'pify';
+import validateOptions from 'schema-utils';
 import processResult from './processResult';
 import getOptions from './getOptions';
+import schema from './options.json';
 
 const render = pify(less.render.bind(less));
 
 function lessLoader(source) {
   const loaderContext = this;
   const options = getOptions(loaderContext);
+  validateOptions(schema, options, 'lessLoader');
   const done = loaderContext.async();
   const isSync = typeof done !== 'function';
 
