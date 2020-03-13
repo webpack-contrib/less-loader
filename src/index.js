75714116f25282b3a65d074ac20576bb9aa43178
Ryan Clark <ryan.clark@udemy.com>
2020-03-13 17:00:14 +0000

    feat: move less-specific options to lessOptions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
+import clone from 'clone';
 import less from 'less';
 import pify from 'pify';
 
+import { getOptions } from 'loader-utils';
+import validateOptions from 'schema-utils';
+
+import schema from './options.json';
 import processResult from './processResult';
-import getOptions from './getOptions';
+import createWebpackLessPlugin from './createWebpackLessPlugin';
 
 const render = pify(less.render.bind(less));
 
 function lessLoader(source) {
-  const loaderContext = this;
-  const options = getOptions(loaderContext);
-  const done = loaderContext.async();
+  const options = getOptions(this) || {};
+
+  validateOptions(schema, options, {
+    name: 'Less Loader',
+    baseDataPath: 'options',
+  });
+
+  const done = this.async();
   const isSync = typeof done !== 'function';
 
   if (isSync) {
@@ -18,7 +28,30 @@ function lessLoader(source) {
     );
   }
 
-  processResult(loaderContext, render(source, options));
+  const lessOptions = {
+    plugins: [],
+    relativeUrls: true,
+    // We need to set the filename because otherwise our WebpackFileManager will receive an undefined path for the entry
+    filename: this.resourcePath,
+    ...(options.lessOptions ? clone(options.lessOptions) : {}),
+  };
+
+  if (typeof lessOptions.paths === 'undefined') {
+    lessOptions.plugins.push(createWebpackLessPlugin(this));
+  }
+
+  if (options.sourceMap) {
+    if (typeof options.sourceMap === 'boolean') {
+      lessOptions.sourceMap = {};
+    }
+
+    if (typeof options.sourceMap.outputSourceFiles === 'undefined') {
+      // Include source files as `sourceContents` as sane default since this makes source maps "just work" in most cases
+      lessOptions.sourceMap.outputSourceFiles = true;
+    }
+  }
+
+  processResult(this, render(source, lessOptions));
 }
 
 export default lessLoader;
