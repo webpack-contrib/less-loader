737767356c4f464c9adb8a49ed591ec194dfb8cd
cap-Bernardito <cap-Bernardito@yandex.ru>
2020-04-22 14:26:36 +0300

    refactor: moving processResult in index

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import { getOptions } from 'loader-utils';
 import validateOptions from 'schema-utils';
 
 import schema from './options.json';
-import processResult from './processResult';
 import getLessOptions from './getLessOptions';
 
 const render = promisify(less.render.bind(less));
+const removeSourceMappingUrl = require('./removeSourceMappingUrl');
+const formatLessError = require('./formatLessError');
 
 function lessLoader(source) {
   const options = getOptions(this) || {};
@@ -22,7 +23,27 @@ function lessLoader(source) {
   const callback = this.async();
   const lessOptions = getLessOptions(this, options, source);
 
-  processResult(this, render(lessOptions.data, lessOptions), callback);
+  render(lessOptions.data, lessOptions)
+    .then(
+      ({ css, map, imports }) => {
+        imports.forEach(this.addDependency, this);
+        return {
+          // Removing the sourceMappingURL comment.
+          // See removeSourceMappingUrl.js for the reasoning behind this.
+          css: removeSourceMappingUrl(css),
+          map: typeof map === 'string' ? JSON.parse(map) : map,
+        };
+      },
+      (lessError) => {
+        if (lessError.filename) {
+          this.addDependency(lessError.filename);
+        }
+        throw formatLessError(lessError);
+      }
+    )
+    .then(({ css, map }) => {
+      callback(null, css, map);
+    }, callback);
 }
 
 export default lessLoader;
