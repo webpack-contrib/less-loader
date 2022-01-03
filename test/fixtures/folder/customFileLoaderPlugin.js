dfb6d28d6fb3eb259114c79cd76e7fcc763042e7
evilebottnawi <sheo13666q@gmail.com>
2022-01-03 16:24:48 +0300

    ci: fix

--- test/fixtures/folder/customFileLoaderPlugin.js
+++ test/fixtures/folder/customFileLoaderPlugin.js
@@ -6,12 +6,22 @@ class Plugin extends less.FileManager {
     if (filename === 'forFileLoaderPluginResolve.less') {
       return true;
     }
+    
+    if (filename === "https://fonts.googleapis.com/css?family=Roboto:500") {
+      return true;
+    }
 
     return false;
   }
 
   loadFile(filename, ...args) {
-    const result = path.resolve(__dirname, '../', 'file-load-replacement.less');
+    let result;
+    
+    if (filename === 'forFileLoaderPluginResolve.less') {
+      result = path.resolve(__dirname, '../', 'file-load-replacement.less');
+    } else if (filename === "https://fonts.googleapis.com/css?family=Roboto:500") {
+      result = path.resolve(__dirname, '../', 'mock-fonts.less');
+    }
 
     return super.loadFile(result, ...args);
   };
